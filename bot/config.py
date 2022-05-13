aab1cbd3ef602188bff434e909d7e27a18b2278a
IAmUnknown24671 <103492310+IAmUnknown24671@users.noreply.github.com>
2022-05-14 01:15:46 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,16 +1,10 @@
-#    Copyright (c) 2021 Danish_00
-#    Improved By @Zylern
-
-
-
-
-    APP_ID = 13484212
-    API_HASH = 8c90d377feee94f58463a4e17030aa02
-    BOT_TOKEN = 5251819077:AAHAEPR73ybk5nJ8YbpaSW48D5z1O00t-u8
-    DEV = 1664850827
-    OWNER = 5145714881
-    ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMBNAIL = https://telegra.ph/file/f9e5d783542906418412d.jpg
+APP_ID = 13484212
+API_HASH = 8c90d377feee94f58463a4e17030aa02
+BOT_TOKEN = 5201695905:AAELYhTXxrEkx8jGANUPYW-8BgkPgFsb5Qk
+DEV = 1664850827
+OWNER = 5145714881
+ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
+THUMBNAIL = https://telegra.ph/file/f9e5d783542906418412d.jpg
 
 
 
